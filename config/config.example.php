0052f11bd9c597dd9cfb27d7d5688687476df0c0
reztechcode <irezabdullah@rezweb.my.id>
2024-02-09 14:56:05 +0700

    update gitignore

--- config/config.example.php
+++ config/config.example.php
@@ -1,5 +1,5 @@
 <?php 
-define('BASE_URL', 'http://localhost/si-survey-be/');
+define('BASE_URL', 'http://localhost/si-survey/');
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
